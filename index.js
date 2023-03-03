813684f96cc39a5a45404860a6ef8c272486b5f3
Adeniyi Janet <adeniyijanet61@gmail.com>
2023-03-03 11:07:45 +0100

    javaScript dayOne

---
+++ index.js
@@ -0,0 +1,51 @@
+// console.log("hello world!");
+
+/*
+ document.write("hello world!");
+ document.write("hello world!");
+ 
+ ssdfggddvcvffddhufjipiuhfnjn
+ */
+
+let name1 = "janet";
+console.log(name1);
+name1 = "seyi";
+name1 = "bola";
+console.log(name1);
+let name = "jumoke";
+
+const pie = 3.142;
+console.log(pie);
+let total = pie * 2;
+console.log(total);
+let number = "2";
+total = number * 1;
+console.log(total);
+let number2;
+console.log(number2);
+
+let user = "Doe";
+let z = 1;
+console.log(typeof user);
+console.log(typeof z);
+let answer1 = "It's alright";
+console.log(answer1);
+let answer2 = 'He is called "Johnny"';
+console.log(answer2);
+
+total = number + 1;
+console.log(total);
+let answer = answer1 + " " + answer2;
+let answers = `${answer1} ${answer2}`;
+console.log(answer);
+console.log(answers);
+
+let bool = true;
+let x, y, b;
+x = 2;
+y = 2;
+b = 3;
+console.log(x == b);
+console.log(typeof bool);
+let c;
+console.log(c);
